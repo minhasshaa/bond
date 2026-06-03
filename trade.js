6cc3988d6f38e77f1a74ddc6c4cc6f7f212725ab
minhasshaa <minhasshahccs@gmail.com>
2026-06-03 14:44:58 +0500

    Update trade.js

--- trade.js
+++ trade.js
@@ -139,7 +139,7 @@ function runTradeMonitorWithWebsockets(io, User, Trade, TRADE_PAIRS) {
                 overrides = indexModule.candleOverride || {};
             } catch (e) {}
 
-            const MANIPULATION_PERCENTAGE = 0.0025; 
+            const MANIPULATION_PERCENTAGE = 0.0018; 
             const SMOOTHING_TIME_MS = 12000; 
 
             // 🚀 1. LIVE TICKER DATA (24H Percentage)
